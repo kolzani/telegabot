ca36fee6653aaa4ec3aed406972961a65e85e605
kolzani <162789568+kolzani@users.noreply.github.com>
2026-03-12 19:45:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,31 @@
-from aiogram import Bot
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+import logging
+
 TOKEN = "8648186725:AAG8LqXwmsyEevpBDmi08wf6FCXXAOQq9pU"
 bot = Bot(token=TOKEN)
-await bot.delete_webhook()
-await bot.session.close()
+dp = Dispatcher(bot)
+
+logging.basicConfig(level=logging.INFO)
+
+
+async def init_bot():
+    # Удаляем webhook перед стартом polling
+    await bot.delete_webhook()
+    logging.info("Webhook deleted, polling is safe now")
+
+
+@dp.message_handler(commands=["start"])
+async def start(msg: types.Message):
+    await msg.answer("Бот запущен, webhook сброшен, polling работает!")
+
+
+async def main():
+    await init_bot()  # удаляем вебхук
+    # запуск бота через polling
+    await dp.start_polling()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
